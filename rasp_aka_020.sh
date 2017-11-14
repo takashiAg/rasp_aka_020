b4bb3dbf996621ff2bfbc5b32d218f45a3e3476a
安藤亮介 <crawd4274@gmail.com>
2017-11-14 10:11:26 +0900

    11th commit

--- rasp_aka_020.sh
+++ rasp_aka_020.sh
@@ -5,7 +5,7 @@ echo -e '[Dialer Defaults]\nInit1 = ATZ\nInit2 = AT+CFUN=1\nInit3 = AT+CGDCONT=1
 echo -e 'noauth\nname wvdial\nusepeerdns\nreplacedefaultroute\n' | sudo tee /etc/ppp/peers/wvdial
 
 cat /lib/udev/rules.d/40-usb_modeswitch.rules |sed -e '$d' |sudo tee /lib/udev/rules.d/40-usb_modeswitch.rules
-echo -e "#ABIT AK-020\nATTRS{idVendor}==\"15eb\", ATTRS{idProduct}==\"a403\", RUN+=\"usb_modeswitch \'%b/%k\'\"\n\nLABEL=\"modeswitch_rules_end\""|sudo tee -a /lib/udev/rules.d/40-usb_modeswitch.rules
+echo -e "#ABIT AK-020\nATTRS{idVendor}==\"15eb\", ATTRS{idProduct}==\"a403\", RUN+=\"usb_modeswitch '%b/%k'\"\n\nLABEL=\"modeswitch_rules_end\""|sudo tee -a /lib/udev/rules.d/40-usb_modeswitch.rules
 #sudo sed '/^LABEL="modeswitch_rules_end"/i #ABIT AK-020\nATTRS{idVendor}==\"15eb\", ATTRS{idProduct}==\"a403\", RUN+=\"usb_modeswitch "%b/%k"\n' /lib/udev/rules.d/40-usb_modeswitch.rules | sudo tee /lib/udev/rules.d/40-usb_modeswitch.rules
 
 #cat /lib/udev/rules.d/40-usb_modeswitch.rules |sed -e '$d'|
